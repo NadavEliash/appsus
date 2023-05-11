246b3e4b0c467cab5434fea9fa42b243c3dec21a
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-12 02:29:35 +0300

    img test

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -2,8 +2,10 @@ export function MailPreview({mail}) {
     return (
         <article className="mail-title">
             <h2 className="from">{mail.from}</h2>
+            <div className="mail-content">
             <h3 className="subject">{mail.subject}</h3>
             <p className="msg">{mail.massage}</p>
+            </div>
             <p className="date">{mail.date}</p>
         </article>
     )
