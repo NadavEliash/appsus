77ac58c35450d78c220cecb9f50b412e486398e2
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-11 20:45:26 +0300

    undo changes of img

--- apps/mail/cmps/mail-filter.jsx
+++ apps/mail/cmps/mail-filter.jsx
@@ -27,9 +27,9 @@ export function MailFilter({ filterBy, onSetFilter, onSetSortBy }) {
         <section className="mail-filter full main-layout">
             <form className="search-box" onSubmit={onSubmitFilter}>
                 <label htmlFor="txt"></label>
-                <img src="../../../assets/img/magnifying-glass.svg" alt="" />
+                <img src="../../assets/img/magnifying-glass.svg" alt="" />
                 <input value={txt} onChange={handleChange} name="txt" id="txt" type="text" placeholder="search emails" />
-                <button onClick={cancelSearch}><img src="../../../assets/img/X.svg" alt="" /></button>
+                <button onClick={cancelSearch}><img src="../../assets/img/X.svg" alt="" /></button>
             </form>
             <button onClick={() => onSetSortBy('date')}>by Date</button>
             <button onClick={() => onSetSortBy('subject')}>by Subject</button>
