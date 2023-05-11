e47792235158647ac1265f1f0d6a29a7b4817dc9
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-11 20:33:13 +0300

    test

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -19,18 +19,18 @@ export function MailList({ mails, onStarred, onLabeled, onRemoveMail }) {
             {mails.map(mail =>
                 <li className={`mail-row ${mail.isRead}`} key={mail.id}>
                     <button onClick={() => onStarred(mail)} className="star-button">
-                        <img className={`empty-star ${mail.isStarred}`} src="./../assets/img/star.svg" alt="" />
-                        <img className={`yellow-star ${mail.isStarred}`} src="./../assets/img/star-yellow.svg" alt="" />
+                        <img className={`empty-star ${mail.isStarred}`} src="../../assets/img/star.svg" alt="" />
+                        <img className={`yellow-star ${mail.isStarred}`} src="../../assets/img/star-yellow.svg" alt="" />
                     </button>
                     <button onClick={() => onLabeled(mail)} className="label-button">
-                        <img className={`empty-label ${mail.isImportant}`} src="./../assets/img/label.svg" alt="" />
-                        <img className={`yellow-label ${mail.isImportant}`} src="./../assets/img/yellow-label.svg" alt="" />
+                        <img className={`empty-label ${mail.isImportant}`} src="../../assets/img/label.svg" alt="" />
+                        <img className={`yellow-label ${mail.isImportant}`} src="../../assets/img/yellow-label.svg" alt="" />
                     </button>
                     <Link onClick={() => { onRead(mail.id) }} className={`mail-row ${mail.isRead}`} to={`/mail/${mail.id}`} >
                         <MailPreview mail={mail} />
                     </Link>
                     <button className="delete-button" onClick={() => onRemoveMail(mail.id)} >
-                        <img src="./../assets/img/delete.svg" alt="" />
+                        <img src="../assets/img/delete.svg" alt="" />
                     </button>
                 </li>
             )}
