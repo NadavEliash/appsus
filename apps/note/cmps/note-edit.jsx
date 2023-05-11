a2283a3a1efa96d90dfe4889d0f0154ea9a41080
“JacobZhigalov” <jzhigalov@gmail.com>
2023-05-11 13:56:42 +0300

    Delivery Thursday 14:00 Jacob

--- apps/note/cmps/note-edit.jsx
+++ apps/note/cmps/note-edit.jsx
@@ -3,52 +3,54 @@ const { useParams, useNavigate } = ReactRouterDOM
 
 import { noteService } from "../services/note.service.js"
 
-export function NoteEdit() {
+export function NoteEdit( {note}) {
 
-    const [noteToEdit, setNoteToEdit] = useState(noteService.getEmptyNote())
-    const navigate = useNavigate()
-    const params = useParams()
+    // const [noteToEdit, setNoteToEdit] = useState(noteService.getEmptyNote())
+    // const navigate = useNavigate()
+    // const params = useParams()
 
-    useEffect(() => {
-        if (params.noteId) loadNote()
-    }, [])
+    // useEffect(() => {
+    //     if (params.noteId) loadNote()
+    // }, [])
 
-    function loadNote() {
-        noteService.get(params.noteId)
-            .then(setNoteToEdit)
-            .catch(err => {
-                console.log('Had issued in note edit:', err);
-                navigate('/note')
-                showErrorMsg('Note not found!')
-            })
-        console.log(params.noteId)
-    }
-
-
-
-
-    function handleChange({ target }) {
-        const field = target.name
-        const value = target.value
-        setNoteToEdit(prevNote => (
-            { ...prevNote, info: { ...prevNote.info, [field]: value } }))
-    }
-
-    function onSaveNote(ev) {
-        ev.preventDefault()
-        noteService.save(noteToEdit)
-            .then(() => {
-                navigate('/note')
-            })
-    }
+    // function loadNote() {
+    //     noteService.get(params.noteId)
+    //         .then(setNoteToEdit)
+    //         .catch(err => {
+    //             console.log('Had issued in note edit:', err);
+    //             navigate('/note')
+    //             showErrorMsg('Note not found!')
+    //         })
+    //     console.log(params.noteId)
+    // }
 
 
 
-    const { info: { title, txt } } = noteToEdit
+
+    // function handleChange({ target }) {
+    //     const field = target.name
+    //     const value = target.value
+    //     setNoteToEdit(prevNote => (
+    //         { ...prevNote, info: { ...prevNote.info, [field]: value } }))
+    // }
+
+    // function onSaveNote(ev) {
+    //     ev.preventDefault()
+    //     noteService.save(noteToEdit)
+    //         .then(() => {
+    //             navigate('/note')
+    //         })
+    // }
+
+
+
+    // const { info: { title, txt } } = noteToEdit
     return (
         <section className="edit-modal">
             <h2>Edit Note:</h2>
-            <form onSubmit={onSaveNote}>
+            <h4>{note.info.title}</h4>
+            <h6>{note.info.txt}</h6>
+            {/* <form onSubmit={onSaveNote}>
                 <label htmlFor="title">Title:</label>
                 <input required onChange={handleChange} value={title} type="text" name="title" id="title" placeholder="Title:" />
 
@@ -56,7 +58,7 @@ export function NoteEdit() {
                 <input required onChange={handleChange} value={txt} type="text" name="txt" id="txt" placeholder="Text here:" />
 
                 <button>Save Note</button>
-            </form>
+            </form> */}
         </section>
     )
 }
--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,13 +1,25 @@
-// import { NoteEdit } from './note-edit.jsx'
+const { useEffect, useState } = React
+
+import { NoteEdit } from './note-edit.jsx'
 
 export function NotePreview({ note }) {
 
+    const [isEditable, setIsEditable] = useState(false)
+
+    function onEditable(){
+        setIsEditable(true)
+    }
+
+    
 
 
     return (
         <article className="note-preview">
-            <h2>{note.info.title} </h2>
+            <h3>{note.info.title} </h3>
             <p>{note.info.txt}</p>
+            <button onClick={onEditable}>Edit</button>
+            {isEditable && <NoteEdit  note={note}  />}
+            
         </article>
     )
 }
