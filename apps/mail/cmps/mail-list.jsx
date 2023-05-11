687eb61e3d481986cedb55193429bcdc3d8a940e
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-11 09:51:26 +0300

    fix ing links and rem size

--- apps/mail/cmps/mail-list.jsx
+++ apps/mail/cmps/mail-list.jsx
@@ -13,19 +13,19 @@ export function MailList({ mails, onStarred, onLabeled, onRemoveMail }) {
             {mails.map(mail =>
                 <li className="mail-row" key={mail.id}>
                     <button onClick={()=>onStarred(mail)} className="star-button">
-                        <img className="empty-star" src="../../assets/img/star.svg" alt="" />
-                        <img className="yellow-star" src="../../assets/img/star-yellow.svg" alt="" />
+                        <img className="empty-star" src="../../../assets/img/star.svg" alt="" />
+                        <img className="yellow-star" src="../../../assets/img/star-yellow.svg" alt="" />
                     </button>
                     <button onClick={()=>onLabeled(mail)} className="label-button">
-                        <img className="empty-label" src="../../assets/img/label.svg" alt="" />
-                        <img className="yellow-label" src="../../assets/img/yellow-label.svg" alt="" />
+                        <img className="empty-label" src="../../../assets/img/label.svg" alt="" />
+                        <img className="yellow-label" src="../../../assets/img/yellow-label.svg" alt="" />
                     </button>
 
                     <Link className="mail-row" to={`/mail/${mail.id}`} >
                         <MailPreview mail={mail} />
                     </Link>
                     <button className="delete-button" onClick={() => onRemoveMail(mail.id)} >
-                        <img src="../../assets/img/delete.svg" alt="" />
+                        <img src="../../../assets/img/delete.svg" alt="" />
                     </button>
                 </li>
             )}
