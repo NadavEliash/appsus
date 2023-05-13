e476e832c65289f167942237787e0922cecf3bbf
Nadav Eliash <nadav.eliash@gmail.com>
2023-05-13 23:09:52 +0300

    push design

--- views/home.jsx
+++ views/home.jsx
@@ -1,8 +1,13 @@
+const { NavLink } = ReactRouterDOM
 
 export function Home() {
     return <section className="home">
-        <h1>Welcome to Appsus!</h1>
+        <h1>Welcome Appsus!</h1>
         <h2>Handle your words carefully,<br />for words have more power than atom bombs</h2>
-        <img src="assets/img/homepage-01.jpg" alt="" />
+        <img src="assets/img/homepage-02.jpg" alt="" />
+        <div className="link">
+            <NavLink to="/mail"><img className="link-mail" src="assets/img/mail.svg" alt="" /></NavLink>
+            <NavLink to="/note"><img className="link-note" src="assets/img/note.svg" alt="" /></NavLink>
+        </div>
     </section>
 }
